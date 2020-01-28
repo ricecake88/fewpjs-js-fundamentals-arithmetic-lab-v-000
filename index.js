2c40d53b580116784fa65fa38935ed0e6201e180
Grace Shih <vocalise@gmail.com>
2020-01-28 07:14:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 // Write your code here
 
-let num1 = 31;
-let num2 = 2;
-let num3 = 40;
-let num4 = 9;
-let num5 = 10;
-let num6 = 15;
-let num7 = 20;
+const num1 = 31;
+const num2 = 2;
+const num3 = 40;
+const num4 = 9;
+const num5 = 10;
+const num6 = 15;
+const num7 = 20;
 
 let multiply = num1 * num2;
-let random = Math.random();
+let random = (Math.random() * 1000)+1;
 let mod = num3 % num4;
 let max = Math.max(num5, num6, num7)
