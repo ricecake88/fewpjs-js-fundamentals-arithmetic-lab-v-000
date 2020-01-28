d6b3faea09b4335ea233ac2282057083cc06ba20
Grace Shih <vocalise@gmail.com>
2020-01-28 07:14:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 // Write your code here
 
-let num1 = 31;
-let num2 = 2;
-let num3 = 40;
-let num4 = 9;
-let num5 = 10;
-let num6 = 15;
-let num7 = 20;
+const num1 = 31;
+const num2 = 2;
+const num3 = 40;
+const num4 = 9;
+const num5 = 10;
+const num6 = 15;
+const num7 = 20;
 
 let multiply = num1 * num2;
 let random = (Math.random() * 1000)+1;
