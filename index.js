ff7c5c82caae2abbcf08d7e398c159d5efbdedce
Grace Shih <vocalise@gmail.com>
2020-01-28 07:12:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 40;
+let num4 = 9;
+let num5 = 10;
+let num6 = 15;
+let num7 = 20;
+
+let multiply = num1 * num2;
+let random = Math.random();
+let mod = num3 % num4;
+let max = Math.max(num5, num6, num7)
